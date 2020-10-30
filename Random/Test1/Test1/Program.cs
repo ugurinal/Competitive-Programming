bb1e87c006fc988bc2c29f307444606595918e3f
ugurinal <ugurinal01@hotmail.com>
2020-10-30 21:03:07 +0300

    test1
    test1

---
+++ Random/Test1/Test1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Test1
+{
+    public class Base
+    {
+        public Base()
+        {
+            Console.WriteLine("Base constructer called!");
+        }
+
+        public Base(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Derived : Base
+    {
+        public Derived() : base("hello")
+        {
+            Console.WriteLine("Derived constructer called!");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            Derived derived = new Derived();
+        }
+    }
+}
