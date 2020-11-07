0432ca7500527a3200edf3acf0e73833d0561194
ugurinal <ugurinal01@hotmail.com>
2020-11-07 22:42:21 +0300

    new solutions
    new solutions

---
+++ HackerRank/30 Days Of Code/Day1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Day1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+            int x;
+            double y;
+            string z;
+
+            if (int.TryParse(Console.ReadLine(), out x) && double.TryParse(Console.ReadLine(), out y))
+            {
+                z = Console.ReadLine();
+
+                Console.WriteLine("{0}", i + x);
+                Console.WriteLine("{0:F1}", d + y); // f1 is for 8.0 , one decimal
+                Console.WriteLine("{0}", s + z);
+            }
+            else
+            {
+                Console.WriteLine("Enter valid inputs.");
+            }
+
+            // Declare second integer, double, and String variables.
+
+            // Read and save an integer, double, and String to your variables.
+
+            // Print the sum of both integer variables on a new line.
+
+            // Print the sum of the double variables on a new line.
+
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+        }
+    }
+}
---
+++ HackerRank/MarkAndToys/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace MarkAndToys
+{
+    internal class Program
+    {
+        private static int MaximumToys(int[] prices, int k)
+        {
+            int counter = 0;
+
+            int size = prices.Length;
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                bool swapped = false;
+
+                for (int j = 0; j < size - i - 1; j++)
+                {
+                    if (prices[j] < prices[j + 1])
+                    {
+                        int temp = prices[j];
+                        prices[j] = prices[j + 1];
+                        prices[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (k >= prices[size - i - 1])
+                {
+                    k -= prices[size - i - 1];
+                    counter++;
+                }
+                else
+                {
+                    break;
+                }
+
+                if (!swapped)   // array is sorted
+                {
+                    break;
+                }
+            }
+
+            return counter;
+        }
+
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
