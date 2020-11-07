0bdab1b732485b72f1679ce6b19c798c1c00030e
ugurinal <ugurinal01@hotmail.com>
2020-11-07 23:46:16 +0300

    new solution
    new solution

---
+++ HackerRank/CountingValleys/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CountingValleys
+{
+    internal class Program
+    {
+        public static int CountingValleys(int steps, string path)
+        {
+            int uCounter = 0;
+            int valleyCounter = 0;
+
+            for (int i = 0; i < steps; i++)
+            {
+                if (path[i] == 'U')
+                {
+                    uCounter++;
+                    if (uCounter == 0)
+                    {
+                        valleyCounter++;
+                    }
+                }
+                else
+                {
+                    uCounter--;
+                }
+            }
+
+            return valleyCounter;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
