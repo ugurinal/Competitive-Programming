474ead00081917a87e1177f51bec39e685f4a2c4
ugurinal <ugurinal01@hotmail.com>
2020-11-08 15:32:20 +0300

    day2
    day2

---
+++ HackerRank/30 Days Of Code/Day2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day2
+{
+    internal class Program
+    {
+        private static void solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+            int totalCost = Convert.ToInt32(Math.Round((meal_cost + (meal_cost * tip_percent / 100) + (meal_cost * tax_percent / 100))));
+            Console.WriteLine(totalCost);
+        }
+
+        private static void Main(string[] args)
+        {
+            solve(10.25, 17, 5);
+        }
+    }
+}
