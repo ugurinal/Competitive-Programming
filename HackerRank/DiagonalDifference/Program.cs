2909475452e5c6c49bc737998a95cfbfe69720b7
ugurinal <ugurinal01@hotmail.com>
2020-11-11 00:21:32 +0300

    new solution
    new solution

---
+++ HackerRank/DiagonalDifference/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiagonalDifference
+{
+    internal class Program
+    {
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int firstDiagonal = 0;
+            int secondDiagonal = 0;
+
+            int size = arr.Count;
+
+            for (int i = 0; i < size; i++)
+            {
+                firstDiagonal += arr[i][i];
+                secondDiagonal += arr[i][size - i - 1];
+            }
+
+            return Math.Abs(firstDiagonal - secondDiagonal);
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
