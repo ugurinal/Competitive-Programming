0d70244209612da303e6b4009024f82008daf1a9
ugurinal <ugurinal01@hotmail.com>
2020-11-11 01:52:47 +0300

    new solution
    new solution

---
+++ HackerRank/PlusMinus/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PlusMinus
+{
+    internal class Program
+    {
+        private static void plusMinus(int[] arr)
+        {
+            int pos = 0;
+            int neg = 0;
+            int zero = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    pos++;
+                }
+                else if (arr[i] < 0)
+                {
+                    neg++;
+                }
+                else
+                {
+                    zero++;
+                }
+            }
+
+            Console.WriteLine((double)pos / arr.Length);
+            Console.WriteLine((double)neg / arr.Length);
+            Console.WriteLine((double)zero / arr.Length);
+        }
+
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
