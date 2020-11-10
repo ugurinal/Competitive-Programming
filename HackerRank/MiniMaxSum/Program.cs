d2dce9c33a2521cb9306c89e1e4986f88f227f5e
ugurinal <ugurinal01@hotmail.com>
2020-11-11 02:17:43 +0300

    new solution
    new solution

---
+++ HackerRank/MiniMaxSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MiniMaxSum
+{
+    internal class Program
+    {
+        private static void miniMaxSum(int[] arr)
+        {
+            long totalSum = 0;
+            long min = arr[0];
+            long max = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                totalSum += arr[i];
+                if (arr[i] < min)
+                    min = arr[i];
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+
+            Console.WriteLine("{0} {1}", totalSum - max, totalSum - min);
+        }
+
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
