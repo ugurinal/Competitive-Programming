078c8efc7f18b21cd33367971dc69338b55c88d8
ugurinal <ugurinal01@hotmail.com>
2020-11-11 02:04:46 +0300

    new solution
    new solution

---
+++ HackerRank/Staircase/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Staircase
+{
+    internal class Program
+    {
+        private static void staircase(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            staircase(6);
+        }
+    }
+}
