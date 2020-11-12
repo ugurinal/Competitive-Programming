cc5f43504ce33e85e230e68cf50188860ff417e2
ugurinal <ugurinal01@hotmail.com>
2020-11-12 15:52:17 +0300

    new solution
    new solution

--- HackerRank/30 Days Of Code/Day6/Program.cs
+++ HackerRank/30 Days Of Code/Day6/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Linq;
 
 namespace Day6
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string s = Console.ReadLine();
+                string odd = "", even = "";
+
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (j % 2 == 0)
+                        even += s[j];
+                    else
+                        odd += s[j];
+                }
+
+                Console.WriteLine("{0} {1}", even, odd);
+            }
         }
     }
 }
