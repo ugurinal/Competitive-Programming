2eb379704ca9bbc3979852ae5bf9956a7f6259ad
ugurinal <ugurinal01@hotmail.com>
2020-11-12 15:35:58 +0300

    new solution
    new solution

---
+++ HackerRank/30 Days Of Code/Day5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day5
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", n, i, n * i);
+            }
+        }
+    }
+}
