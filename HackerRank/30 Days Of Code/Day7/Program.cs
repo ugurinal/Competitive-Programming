7a47e95235066e9f1b9bd3f87f723a7dca385f5b
ugurinal <ugurinal01@hotmail.com>
2020-11-13 14:10:26 +0300

    new solution
    new solution

---
+++ HackerRank/30 Days Of Code/Day7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day7
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+
+            /*
+            // to reverse the array
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                int temp = arr[i];
+                arr[i] = arr[arr.Length - 1 - i];
+                arr[arr.Length - 1 - i] = temp;
+            }
+            // prints array
+            foreach (int x in arr)
+            {
+                Console.Write(x + " ");
+            }
+            */
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
