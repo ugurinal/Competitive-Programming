11746a66d693cc34fe42b158a44170faf76c6787
ugurinal <ugurinal01@hotmail.com>
2020-11-16 21:40:18 +0300

    Day10 solution
    Day10 solution

---
+++ HackerRank/30 Days Of Code/Day10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day10
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int max = 0;
+            int prev = 0;
+
+            for (int i = 0; n > 0; i++)
+            {
+                int temp = n % 2;
+                n /= 2;
+                if (temp == 1)
+                {
+                    prev++;
+                    if (prev > max)
+                    {
+                        max = prev;
+                    }
+                }
+                else
+                {
+                    prev = 0;
+                }
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
