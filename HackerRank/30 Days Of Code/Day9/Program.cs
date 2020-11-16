8071620cace68ef62822191334aa5cee86eda235
ugurinal <ugurinal01@hotmail.com>
2020-11-16 21:22:59 +0300

    new solution
    new solution

---
+++ HackerRank/30 Days Of Code/Day9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day9
+{
+    internal class Program
+    {
+        private static int factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return factorial(n - 1) * n;
+        }
+
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
