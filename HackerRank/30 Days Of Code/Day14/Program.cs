8faf1235fae8610caa04cdd010863703c1ec553b
ugurinal <ugurinal01@hotmail.com>
2020-11-20 20:06:59 +0300

    Day14 solution
    Day14 solution

---
+++ HackerRank/30 Days Of Code/Day14/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day14
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
