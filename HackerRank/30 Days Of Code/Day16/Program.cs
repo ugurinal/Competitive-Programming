63fc75ce2e0bcd84d16e02d3deef1934b1a26a64
ugurinal <ugurinal01@hotmail.com>
2020-11-28 21:10:36 +0300

    Day16 solution
    Day16 solution

---
+++ HackerRank/30 Days Of Code/Day16/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day16
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            try
+            {
+                int num = int.Parse(S);
+                Console.WriteLine(num);
+            }
+            catch
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+}
