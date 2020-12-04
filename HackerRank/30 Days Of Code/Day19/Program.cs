196a9748fabc82fc41fb1d6b03b976dd28e30665
ugurinal <ugurinal01@hotmail.com>
2020-12-05 00:20:39 +0300

    Day19 solution
    Day19 solution

---
+++ HackerRank/30 Days Of Code/Day19/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day19
+{
+    public interface AdvancedArithmetic
+    {
+        int divisorSum(int n);
+    }
+
+    public class Calculator : AdvancedArithmetic
+    {
+        public int divisorSum(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    sum += i;
+            }
+            return sum;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            AdvancedArithmetic myCalculator = new Calculator();
+            int sum = myCalculator.divisorSum(n);
+            Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+        }
+    }
+}
