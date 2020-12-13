9b370fc58177545cf5811eb25cbf283c9aeabbd2
ugurinal <ugurinal01@hotmail.com>
2020-12-13 03:45:02 +0300

    Day21 solution
    Day21 solution

---
+++ HackerRank/30 Days Of Code/Day21/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day21
+{
+    internal class Program
+    {
+        private static void PrintArray<T>(T[] myArray)
+        {
+            foreach (var obj in myArray)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] stringArray = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                stringArray[i] = Console.ReadLine();
+            }
+
+            PrintArray<Int32>(intArray);
+            PrintArray<String>(stringArray);
+        }
+    }
+}
