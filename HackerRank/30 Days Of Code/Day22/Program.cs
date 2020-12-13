b30a5eda3534b8ef3bb4a7d28e0c5e495846c23a
ugurinal <ugurinal01@hotmail.com>
2020-12-14 00:22:38 +0300

    Day22 solution
    Day22 solution

---
+++ HackerRank/30 Days Of Code/Day22/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Day22
+{
+    internal class Node
+    {
+        public Node left, right;
+        public int data;
+
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    internal class Program
+    {
+        private static int getHeight(Node root)
+        {
+            if (root == null)
+                return -1;
+
+            int left = getHeight(root.left) + 1;
+            int right = getHeight(root.right) + 1;
+
+            return left > right ? left : right;
+        }
+
+        private static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            int height = getHeight(root);
+            Console.WriteLine(height);
+        }
+    }
+}
