b2ba2d0e746ad936a33a2d4f0bfeccc62e331a96
ugurinal <ugurinal01@hotmail.com>
2020-12-16 15:01:34 +0300

    Day23 solution
    Day23 solution

---
+++ HackerRank/30 Days Of Code/Day23/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day23
+{
+    internal class Node
+    {
+        public Node left, right;
+        public int data;
+
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    internal class Program
+    {
+        private static void levelOrder(Node root)
+        {
+            Queue<Node> queue = new Queue<Node>();
+
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                Node node = queue.Dequeue();
+                Console.Write(node.data + " ");
+                if (node.left != null)
+                    queue.Enqueue(node.left);
+                if (node.right != null)
+                    queue.Enqueue(node.right);
+            }
+        }
+
+        private static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            levelOrder(root);
+        }
+    }
+}
