9084c6beb34164dcc4ccdc644630620710e7d042
ugurinal <ugurinal01@hotmail.com>
2020-12-27 02:54:43 +0300

    Day29 solution
    Day29 solution

---
+++ HackerRank/30 Days Of Code/Day29/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day29
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int max = int.MinValue;
+
+                string[] nk = Console.ReadLine().Split(' ');
+
+                int n = Convert.ToInt32(nk[0]);
+
+                int k = Convert.ToInt32(nk[1]);
+
+                for (int i = 1; i < n; i++)
+                {
+                    for (int j = i; j <= n; j++)
+                    {
+                        int temp = i & j;
+                        if (temp > max && temp < k)
+                        {
+                            max = temp;
+                        }
+                    }
+                }
+                Console.WriteLine(max);
+            }
+        }
+    }
+}
