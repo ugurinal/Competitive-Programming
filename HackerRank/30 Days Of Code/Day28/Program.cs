43377158c1605e883b0396d3ed69c29183030629
ugurinal <ugurinal01@hotmail.com>
2020-12-27 02:26:08 +0300

    Day28 solution
    Day28 solution

---
+++ HackerRank/30 Days Of Code/Day28/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
