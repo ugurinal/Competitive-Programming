dd300562b64028fb86d5d48a816e3bc478ec725f
ugurinal <ugurinal01@hotmail.com>
2020-12-27 02:39:08 +0300

    Day28 solution
    Day28 solution

--- HackerRank/30 Days Of Code/Day28/Program.cs
+++ HackerRank/30 Days Of Code/Day28/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System;
 
 namespace Day28
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            List<string> names = new List<string>();
+
+            Regex regex = new Regex("@gmail.com$");
+
+            for (int NItr = 0; NItr < N; NItr++)
+            {
+                string[] firstNameEmailID = Console.ReadLine().Split(' ');
+
+                string firstName = firstNameEmailID[0];
+
+                string emailID = firstNameEmailID[1];
+
+                if (regex.IsMatch(emailID))
+                {
+                    names.Add(firstName);
+                }
+            }
+
+            names.Sort();
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
